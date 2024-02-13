3c54b1ae86da147c85155de76695480deb478401
LorreaLadao <141934519+LorreaLadao@users.noreply.github.com>
2024-02-14 00:23:55 +0800

    first commit

---
+++ src/test/java/com/example/accessingmydatasql/AccessingmydatasqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingmydatasql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingmydatasqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
